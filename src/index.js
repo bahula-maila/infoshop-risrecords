c95346aed5b7ca7c72d0072308add17c54601780
bahulamaila <bahula.maila@gmail.com>
2021-06-14 12:57:04 +0545

    Added Components, Pages, and Branch in config.yml

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const index = () => {
+  return (
+    <>
+      Hello world!
+      <p><Link to="/blog">View Blog</Link></p>
+    </>)
+}
+
+export default index
